9cf80812293c1cc5f5670542b4d6b21da106becc
KashishSethi31 <112857279+KashishSethi31@users.noreply.github.com>
2025-01-15 15:27:13 +0530

    fifth commit

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 print('Master Branch')
 print('Created Dev branch')
 print('made seperate branch')
-<<<<<<< HEAD
 #merged kdev and dev branches
-=======
 print("hellooo")
->>>>>>> 97bfe37 (fourth commit)
